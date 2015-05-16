23fe978270ab0acad6b752ce60e0fd4641f7eae1
Lu Liu <luliu27@gmail.com>
2015-05-16 12:34:01 -0400

    add csv parser dependency, convert json string to array of elements and write to csv

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/data.csv "0.1.2"]
                  [cheshire "5.4.0"]]
   :main json-csv-converter.core)
--- src/json_csv_converter/core.clj
+++ src/json_csv_converter/core.clj
@@ -1,7 +1,8 @@
 (ns json-csv-converter.core
-  (:use [clojure.java.io :only [file reader]])
+  (:use [clojure.java.io :only [file reader writer]])
   (:require [cheshire.core :as json]
             [clojure.string :as string]
+            [clojure.data.csv :as csv]
             [clojure.tools.cli :refer [parse-opts]])
   (:import (java.io FileInputStream)))
 
@@ -40,6 +41,11 @@
        (split-file)
        (map #(string/split % #"\."))))
 
+(defn json-to-csv-row
+  "Convert a map object into array of [key value]"
+  [columns json-map]
+  (map #(get-in json-map %) columns))
+
 (defn -main
   [& args]
   (let [{:keys [options arguments summary errors]} (parse-opts args cli-options)]
@@ -48,9 +54,12 @@
      (nil? (:input options)) (exit 1 (usage summary "Missing Input file"))
      (nil? (:col-spec options)) (exit 1 (usage summary "Missing Column spec"))
      errors (exit 1 (error-msg errors))) 
-    (with-open [s (FileInputStream. (file (:input options)))]
+    (with-open [s (FileInputStream. (file (:input options)))
+                w (writer (or (:output options) (string/join "." [(:input options) "csv"])))]
       (let [r (reader s)
             cols (get-columns (:col-spec options))]
         (do
-          (doall (map prn cols))
-          (doall (map prn (json/parsed-seq r))))))))
+          (csv/write-csv w (vector (map #(string/join "." %) cols))) ;; write header
+          (doall (->> (json/parsed-seq r)                            ;; json string to map
+                      (map #(json-to-csv-row cols %))                ;; flatten and convert
+                      (map #(csv/write-csv w (vector %))))))))))     ;; write to file as csv
