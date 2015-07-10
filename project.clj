6bb4c772daf8e0b48b78c8da668683d9a92c11ce
Lu Liu <luliu27@gmail.com>
2015-07-10 19:28:19 -0400

    upgrade to clojure 1.7; uberjar with :aot

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+  :profiles {:uberjar {:aot :all}}
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/data.csv "0.1.2"]
                  [cheshire "5.4.0"]]
